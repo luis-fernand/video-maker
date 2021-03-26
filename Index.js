f9170f47ec3785e74a49cb86eeac9d3a18639b64
luis-fernand <fernando.r_138@hotmail.com>
2021-03-25 23:02:12 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,12 @@
+import readline from 'readline-sync'
+var readline = require('readline-sync')
 function start() {
     const content = {}
 
     content.searchTerm = askAndReturnSearchTerm()
 
     function askAndReturnSearchTerm() {
-        return 'TERM EXAMPLE'
+        return readline.question('Type a Wikipedia search term: ')
     }
     
     
